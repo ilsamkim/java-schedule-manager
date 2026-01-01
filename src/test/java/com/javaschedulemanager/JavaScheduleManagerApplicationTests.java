bda57b264558b83f6c2fa67703987e9cc1fe5a45
김동진 <kimdj9790@naver.com>
2026-01-02 01:53:57 +0900

    11

---
+++ src/test/java/com/javaschedulemanager/JavaScheduleManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaschedulemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaScheduleManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
